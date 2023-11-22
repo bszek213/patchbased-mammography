1091fb9c7ca6b0a3d42ed57d95b555b114e6d61d
bszek213 <bszekely@nevada.unr.edu>
2023-11-22 09:02:59 -0800

    UPDATE: new model for different kinds of patches

--- patch_based_classify.py
+++ patch_based_classify.py
@@ -185,12 +185,9 @@ def random_patch_abnormal(image,list_mass):
     list_mass = [int(x) for x in list_mass]
     xmin, xmax, ymin, ymax = list_mass[0], list_mass[1], list_mass[2], list_mass[3]
     #iterate until sampled adequately
-    previous_patch = None
     max_attempts = 20   
     attempt = 0
     saved_patches = []
-    lesion_im = image[list_mass[2]:list_mass[3], 
-                        list_mass[0]:list_mass[1]]
     while attempt < max_attempts:
         x_low = uniform(xmin, xmax - window_size)
         x_high = x_low + window_size
@@ -218,8 +215,37 @@ def random_patch_abnormal(image,list_mass):
     for img in saved_patches:
         if img['data'].shape == (window_size, window_size, 3):
             save_lesion.append(img['data'])
-    print(f'length of list {len(saved_patches)}')
+    print(f'length of lesions {len(saved_patches)}')
     return np.array(save_lesion)
+
+def random_patch_normal(image,list_mass):
+    window_size = int(WINDOW_SIZE/2)
+    patches = view_as_windows(image, (window_size, window_size, 3), step=(window_size, window_size, 3))
+    shape_patches = np.shape(patches)
+    xmin, xmax, ymin, ymax = list_mass[0], list_mass[1], list_mass[2], list_mass[3]
+    valid_patches = []
+    for i in range(shape_patches[0]): #iterates over 
+        y_pix_curr = i * window_size
+        x_pix_curr = 0
+        for j in range(shape_patches[1]): #iterates over x
+            x_pix_curr = j * window_size
+            current_patch = patches[i, j]
+            if (
+                x_pix_curr < xmin or x_pix_curr + window_size > xmax or
+                y_pix_curr < ymin or y_pix_curr + window_size > ymax
+            ) and np.mean(current_patch[0] > 40):
+                # print('------')
+                # print(np.mean(current_patch[0]))
+                # print('------')
+                valid_patches.append(current_patch[0])
+                # plt.imshow(current_patch[0])
+                # rect = plt.Rectangle((0, 0), WINDOW_SIZE, WINDOW_SIZE, linewidth=15, edgecolor='g', facecolor='none')
+                # plt.gca().add_patch(rect)
+            else:
+                continue
+            #     plt.imshow(current_patch[0])
+            # plt.show()
+    return np.array(valid_patches)
 
 def is_similar(patch, existing_patches):
     for existing_patch in existing_patches:
@@ -263,37 +289,185 @@ def create_small_medium_large(list_all_values):
         file.write(f"Medium (Median): {q2}\n")
         file.write(f"Large: {q3}\n")
 
+def create_patch_model_res(input_shape):
+    """
+    input shape = (250,250,3,12)
+    """
+    # input_layer = Input(shape=input_shape)
+    # rgb_input = Concatenate()([input_layer, input_layer, input_layer])
+    base_model = ResNet152V2(include_top=False, weights='imagenet', input_shape=(GLOBAL_X, GLOBAL_Y, 3))
+    # base_model = VGG16(weights='imagenet', include_top=False, input_shape=(GLOBAL_X, GLOBAL_Y, 3))
+    model = Sequential()
+    model.add(base_model)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(2, activation="softmax"))
+    model.compile(loss=BinaryFocalCrossentropy(), optimizer='adam', 
+                  metrics=['accuracy'])
+    model.summary()
+    return model
+
+def create_patch_model_dense(input_shape):
+    """
+    input shape = (250,250,3,12)
+    """
+    # input_layer = Input(shape=input_shape)
+    # rgb_input = Concatenate()([input_layer, input_layer, input_layer])
+    base_model = DenseNet121(include_top=False, weights='imagenet', input_shape=(GLOBAL_X, GLOBAL_Y, 3))
+    # base_model = VGG16(weights='imagenet', include_top=False, input_shape=(GLOBAL_X, GLOBAL_Y, 3))
+    model = Sequential()
+    model.add(base_model)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(2, activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', 
+                  metrics=['accuracy'])
+    model.summary()
+
 def main():
-    glob_dir = '/media/brianszekely/TOSHIBA EXT/mammogram_images/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/images'
-    df = read_df()
-    df.dropna(inplace=True)
-    list_all_values = [] 
-    combined_array_lesion = []
-    combined_array = np.empty((0, WINDOW_SIZE, WINDOW_SIZE, 3), dtype=np.uint8)
-    for iteration, (index, row) in enumerate(df.iterrows()):
-            image_type = row['image_id'] + '.dicom'
-            dicom_path = os.path.join(glob_dir,row['study_id'],image_type)
-            if os.path.exists(dicom_path):
-                png_file = convert_dicom_to_png(dicom_path)
-                #abnormal images
-                if row['breast_birads'] > 1:
-                    clahe_image = clahe(png_file)
-                    list_all_values = get_lesion_size([row['xmin'],row['xmax'],row['ymin'],row['ymax']],list_all_values)
-                    lesion_images = random_patch_abnormal(clahe_image,[row['xmin'],row['xmax'],row['ymin'],row['ymax']])
-                    print(np.concatenate(lesion_images).shape)
-                    input()
-                    # combined_array_lesion.append(lesion_images)
-                    print(np.shape(combined_array))
-    
-    #THIS PART IS DONE
-    # #create small medium and large patches
-    # create_small_medium_large(list_all_values)
-    # #plot hist
-    # histplot(list_all_values, bins='auto', kde=True, color='tab:blue')
-    # plt.xlabel('Euclidean Distance of the Bounding Box')
-    # plt.ylabel('Count')
-    # plt.savefig('hist_sizes_lesions.png',dpi=350)
-    # plt.close()
+    if not exists('X_train.npy'):
+        glob_dir = '/media/brianszekely/TOSHIBA EXT/mammogram_images/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/images'
+        df = read_df()
+        df.dropna(inplace=True)
+        list_all_values = [] 
+        selected_arrays = []
+        selected_arrays_non_lesion = []
+        combined_array = np.empty((0, WINDOW_SIZE, WINDOW_SIZE, 3), dtype=np.uint8)
+        for iteration, (index, row) in enumerate(df.iterrows()):
+                image_type = row['image_id'] + '.dicom'
+                dicom_path = os.path.join(glob_dir,row['study_id'],image_type)
+                if os.path.exists(dicom_path):
+                    png_file = convert_dicom_to_png(dicom_path)
+                    #abnormal images
+                    if row['breast_birads'] > 1:
+                        clahe_image = clahe(png_file)
+                        list_all_values = get_lesion_size([row['xmin'],row['xmax'],row['ymin'],row['ymax']],list_all_values)
+                        non_lesion_images = random_patch_normal(clahe_image,[row['xmin'],row['xmax'],row['ymin'],row['ymax']])
+                        lesion_images = random_patch_abnormal(clahe_image,[row['xmin'],row['xmax'],row['ymin'],row['ymax']])
+                        if lesion_images.shape[0] > 0: 
+                            selected_arrays.append(lesion_images)
+                        if non_lesion_images.shape[0] > 0: 
+                            selected_arrays_non_lesion.append(non_lesion_images)
+        combined_array_lesion = np.concatenate(selected_arrays, axis=0)
+        combined_array_non_lesion = np.concatenate(selected_arrays_non_lesion, axis=0)
+        selected_indices = np.random.choice(combined_array_non_lesion.shape[0], size=combined_array_lesion.shape[0], replace=False)
+        combined_array_non_lesion_updated = combined_array_non_lesion[selected_indices]
+        print(np.shape(combined_array_lesion))
+        print(np.shape(combined_array_non_lesion_updated))
+        non_lesion_labels = np.zeros(combined_array_non_lesion_updated.shape[0])
+        lesion_labels = np.ones(combined_array_lesion.shape[0])
+        labels = to_categorical(np.concatenate((non_lesion_labels, lesion_labels)), 
+                                                num_classes=2, dtype="int")
+        features = np.concatenate((combined_array_non_lesion_updated,combined_array_lesion), axis=0)
+
+        X_train, X_temp, y_train, y_temp = train_test_split(features, labels, test_size=0.4, random_state=42)
+        X_validation, X_test, y_validation, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
+
+        # Save the data
+        np.save('X_train.npy', X_train)
+        np.save('X_validation.npy', X_validation)
+        np.save('X_test.npy', X_test)
+        np.save('y_train.npy', y_train)
+        np.save('y_validation.npy', y_validation)
+        np.save('y_test.npy', y_test)
+
+        #THIS PART IS DONE
+        # #create small medium and large patches
+        # create_small_medium_large(list_all_values)
+        # #plot hist
+        # histplot(list_all_values, bins='auto', kde=True, color='tab:blue')
+        # plt.xlabel('Euclidean Distance of the Bounding Box')
+        # plt.ylabel('Count')
+        # plt.savefig('hist_sizes_lesions.png',dpi=350)
+        # plt.close()
+    else:
+        X_train = np.load('X_train.npy')
+        X_test = np.load('X_test.npy')
+        y_train = np.load('y_train.npy')
+        y_test = np.load('y_test.npy')
+        x_val = np.load('X_validation.npy')
+        y_val = np.load('y_validation.npy')
+
+    print(f'x_train size {np.shape(X_train)}')
+    print(f'X_test size {np.shape(X_test)}')
+    print(f'y_train size {np.shape(y_train)}')
+    print(f'y_test size {np.shape(y_test)}')
+    print(f'x_val size {np.shape(x_val)}')
+    print(f'y_val size {np.shape(y_val)}')
+    patch_architecture_dense = create_patch_model_dense((int(WINDOW_SIZE/2),int(WINDOW_SIZE/2),3))
+    plt.figure(figsize=(15, 8))
+    previous_val_acc = 0
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+    print('Train denseNet')
+    if not exists('patch_DenseNet121_small_patch.h5'):
+        for i in range(1):
+            history = patch_architecture_dense.fit(X_train, y_train, epochs=50, batch_size=64,callbacks=[early_stopping],
+                                        validation_data=(x_val, y_val), verbose=1)
+
+            plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+            plt.plot(history.history['accuracy'], label=f'Training Accuracy ({i}th iteration)')
+            plt.plot(history.history['val_accuracy'], label=f'Validation Accuracy ({i}th iteration)')
+            #plt.plot(history.history['val_f1_score'], label=f'Validation F1 Score ({i}th iteration)')
+            plt.title('DenseNet Baseline Model Accuracy History')
+            plt.xlabel('Epoch')
+            plt.ylabel('Accuracy')
+            plt.legend()
+
+            plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+            plt.plot(history.history['loss'], label=f'Training Loss ({i} iteration)')
+            plt.plot(history.history['val_loss'], label=f'Validation Loss ({i} iteration)')
+            plt.title('Densenet Baseline Model Loss History Small Patches')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.legend()
+            if history.history['val_accuracy'][-1] > previous_val_acc:
+                save_patch_model = patch_architecture_dense
+                print(f'({i} iteration) best model: {history.history["val_accuracy"][-1]}')
+        plt.tight_layout()  # Adjust subplot spacing for better appearance
+        plt.savefig('training_accuracy_loss_DenseNet_small_patch.png', dpi=400)
+        test_results = patch_architecture_dense.evaluate(X_test, y_test)
+        print(f'Test Accuracy: {test_results[1]}')
+        print(f'Test Loss: {test_results[0]}')
+        with open('test_results_dense.txt', 'w') as file:
+            file.write(f'Test Accuracy: {test_results[1]}\n')
+            file.write(f'Test Loss: {test_results[0]}\n')
+        save_model(save_patch_model,'patch_DenseNet121_small_patch.h5')
+
+    if not exists('patch_ResNet152_small_patch.h5'):
+        print('Train ResNet')
+        previous_val_acc = 0
+        patch_architecture_res = create_patch_model_res((int(WINDOW_SIZE/2),int(WINDOW_SIZE/2),3))
+        for i in range(1):
+            history = patch_architecture_res.fit(X_train, y_train, epochs=50, batch_size=64,callbacks=[early_stopping],
+                                            validation_data=(x_val, y_val), verbose=1)
+
+            plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+            plt.plot(history.history['accuracy'], label=f'Training Accuracy ({i}th iteration)')
+            plt.plot(history.history['val_accuracy'], label=f'Validation Accuracy ({i}th iteration)')
+            #plt.plot(history.history['val_f1_score'], label=f'Validation F1 Score ({i}th iteration)')
+            plt.title('ResNet152 Baseline Model Accuracy History Small Patches')
+            plt.xlabel('Epoch')
+            plt.ylabel('Accuracy')
+            plt.legend()
+
+            plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
+            plt.plot(history.history['loss'], label=f'Training Loss ({i} iteration)')
+            plt.plot(history.history['val_loss'], label=f'Validation Loss ({i} iteration)')
+            plt.title('ResNet152 Baseline Model Loss History')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.legend()
+            if history.history['val_accuracy'][-1] > previous_val_acc:
+                save_patch_model = patch_architecture_res
+                print(f'({i} iteration) best model: {history.history["val_accuracy"][-1]}')
+
+        plt.tight_layout()  # Adjust subplot spacing for better appearance
+        plt.savefig('training_accuracy_loss_ResNet152_small_patches.png', dpi=400)
+        test_results = patch_architecture_res.evaluate(X_test, y_test)
+        with open('test_results_res.txt', 'w') as file:
+            file.write(f'Test Accuracy: {test_results[1]}\n')
+            file.write(f'Test Loss: {test_results[0]}\n')
+        save_model(save_patch_model,'patch_ResNet152_small_patches.h5')
+
     
 if __name__ == "__main__":
     main()
