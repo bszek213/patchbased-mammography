39c84b877f21b1e9b35be901a742c7e6ac58bc3e
bszek213 <bszekely@nevada.unr.edu>
2023-11-30 10:44:25 -0800

    ADD: better handling of patch generation

--- create_data.py
+++ create_data.py
@@ -1,61 +1,17 @@
-#CAM MAMMOGRAM
-from tensorflow.keras.applications.resnet_v2 import ResNet152V2
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.regularizers import L2
-# from tensorflow.keras.models import Model
-from tensorflow.keras import layers
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense#, Input, Concatenate
-# from tensorflow.keras.models import Model
-from tensorflow.keras.applications import DenseNet121
-# from keras.applications.vgg16 import VGG16
-# from keras.layers import Dense#, Dropout, Flatten, BatchNormalization
-# import tensorflow as tf
-from tensorflow.keras.losses import BinaryFocalCrossentropy
-from tensorflow.keras.utils import to_categorical
-from keras.models import Sequential
 import matplotlib.pyplot as plt
 import pydicom
 import numpy as np
 import os
 import cv2
 from pandas import read_csv
-# from sklearn.feature_extraction.image import PatchExtractor
-# from patchify import patchify
-# from np_utils import to_categorical
-from sklearn.model_selection import train_test_split
-# from statistics import mode
-# import pickle
-from skimage.util import view_as_windows
-from tensorflow.keras.models import save_model#, load_model
-# from tensorflow.keras.metrics import F1Score
-from tensorflow.keras.callbacks import EarlyStopping
+from sys import argv
 from os.path import exists
 from math import sqrt
-# from seaborn import histplot
-from random import uniform
-# import matplotlib.patches as patches
-# from skimage.metrics import structural_similarity as ssim
-# from pandas import DataFrame
-# from itertools import chain
-from sys import argv
-"""
-BIRADS Categories
+from random import uniform, sample
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from skimage.util import view_as_windows
 
-Category 0: Incomplete assessment - Additional imaging is needed for a complete evaluation. This may be due to technical issues or incomplete imaging.
-
-Category 1: Negative - No significant findings; the breast is entirely normal.
-
-Category 2: Benign - There are findings indicative of benign (non-cancerous) conditions, such as cysts or benign tumors.
-
-Category 3: Probably benign - The findings have a very low probability of being cancerous. Follow-up imaging may be recommended to monitor changes.
-
-Category 4: Suspicious - There is a moderate to high suspicion of malignancy. Further evaluation, such as a biopsy, is usually recommended.
-
-Category 5: Highly suggestive of malignancy - The findings are highly suspicious of being cancerous. Immediate action, like a biopsy, is usually recommended.
-
-Category 6: Known biopsy-proven malignancy - The cancer has already been confirmed through a biopsy.
-"""
 if argv[1] == 'small':
     WINDOW_SIZE = 222
     append_name = 'small'
@@ -71,6 +27,7 @@ def read_df(path="/media/brianszekely/TOSHIBA EXT/mammogram_images/vindr-mammo-a
     df['breast_birads'] = df['breast_birads'].str.replace('BI-RADS ', '')
     df['breast_birads'] = df['breast_birads'].astype(int)
     return df[['study_id', 'image_id','view_position','breast_birads','xmin','xmax','ymin','ymax']]
+
 def convert_dicom_to_png(dicom_file: str) -> np.ndarray:
     """
     dicom_file: path to the dicom fife
@@ -216,7 +173,7 @@ def random_patch_abnormal(image,list_mass):
     list_mass = [int(x) for x in list_mass]
     xmin, xmax, ymin, ymax = list_mass[0], list_mass[1], list_mass[2], list_mass[3]
     #iterate until sampled adequately
-    max_attempts = 5   
+    max_attempts = 25
     attempt = 0
     saved_patches = []
     while attempt < max_attempts:
@@ -286,10 +243,12 @@ def random_patch_abnormal(image,list_mass):
             saved_patches.append(patch)
         attempt += 1
     save_lesion = []
+    image_num = 0
     for img in saved_patches:
         if img['data'].shape == (window_size, window_size, 3):
             save_lesion.append(img['data'])
-    print(f'length of lesions {len(saved_patches)}')
+            image_num += 1
+    print(f'length of lesions {image_num}')
     return np.array(save_lesion)
 
 def random_patch_normal(image,list_mass):
@@ -363,102 +322,6 @@ def create_small_medium_large(list_all_values):
         file.write(f"Medium (Median): {q2}\n")
         file.write(f"Large: {q3}\n")
 
-def create_patch_model_res(input_shape):
-    """
-    input shape = (250,250,3,12)
-    """
-    # input_layer = Input(shape=input_shape)
-    # rgb_input = Concatenate()([input_layer, input_layer, input_layer])
-    base_model = ResNet152V2(include_top=False, weights='imagenet', input_shape=(int(WINDOW_SIZE/2), int(WINDOW_SIZE/2), 3))
-    # base_model = VGG16(weights='imagenet', include_top=False, input_shape=(GLOBAL_X, GLOBAL_Y, 3))
-    model = Sequential()
-    model.add(base_model)
-    model.add(GlobalAveragePooling2D())
-    model.add(Dense(2, activation="softmax"))
-    model.compile(loss=BinaryFocalCrossentropy(), optimizer='adam', 
-                  metrics=['accuracy'])
-    model.summary()
-    return model
-
-def create_patch_model_dense(input_shape):
-    """
-    input shape = (250,250,3,12)
-    """
-    # input_layer = Input(shape=input_shape)
-    # rgb_input = Concatenate()([input_layer, input_layer, input_layer])
-    base_model = DenseNet121(include_top=False, weights='imagenet', input_shape=(int(WINDOW_SIZE/2), int(WINDOW_SIZE/2), 3))
-    # base_model = VGG16(weights='imagenet', include_top=False, input_shape=(GLOBAL_X, GLOBAL_Y, 3))
-    model = Sequential()
-    model.add(base_model)
-    model.add(GlobalAveragePooling2D())
-    model.add(Dense(2, activation="softmax"))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', 
-                  metrics=['accuracy'])
-    model.summary()
-    return model
-
-def create_patch_custom_model():
-    #Define the CNN model
-    model = Sequential()
-
-    #Input layer
-    model.add(layers.InputLayer(input_shape=(int(WINDOW_SIZE/2), int(WINDOW_SIZE/2), 3)))
-
-    #Convolutional Block 1
-    model.add(layers.ZeroPadding2D(padding=(3, 3)))
-    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
-    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    #Convolutional Block 2
-    model.add(layers.Conv2D(64, (1, 1), activation='relu'))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    #Convolutional Block 3
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    #Convolutional Block 4
-    model.add(layers.Conv2D(256, (1, 1), activation='relu'))
-    model.add(layers.Conv2D(256, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    #Convolutional Block 5
-    model.add(layers.Conv2D(512, (3, 3), activation='relu'))
-    model.add(layers.Conv2D(512, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    #Convolutional Block 6
-    model.add(layers.Conv2D(1024, (1, 1), activation='relu'))
-    model.add(layers.Conv2D(1024, (3, 3), activation='relu'))
-    model.add(layers.ZeroPadding2D(padding=(1, 1)))
-    model.add(layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    #Global Average Pooling and Flatten
-    model.add(layers.GlobalAveragePooling2D())
-    model.add(layers.Flatten())
-
-    #Dropout layer
-    model.add(layers.Dropout(0.5))
-
-    #Dense layer
-    model.add(layers.Dense(2, activation='softmax', kernel_regularizer=L2(0.0002)))
-
-    # Print model summary
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), 
-                  metrics=['accuracy'])
-    model.summary()
-
-    return model 
-
-
 def main():
     if not exists(f'X_train_{append_name}.npy'):
         glob_dir = '/media/brianszekely/TOSHIBA EXT/mammogram_images/vindr-mammo-a-large-scale-benchmark-dataset-for-computer-aided-detection-and-diagnosis-in-full-field-digital-mammography-1.0.0/images'
@@ -480,10 +343,13 @@ def main():
                         for ind_image in clahe_image:
                             non_lesion_images = random_patch_normal(ind_image,[row['xmin'],row['xmax'],row['ymin'],row['ymax']])
                             lesion_images = random_patch_abnormal(ind_image,[row['xmin'],row['xmax'],row['ymin'],row['ymax']])
-                            if lesion_images.shape[0] > 0: 
+                            if lesion_images.shape[0] > 0:
+                                ran_select = np.random.choice(non_lesion_images.shape[0], size=lesion_images.shape[0], replace=False)
+                                non_lesion_images = non_lesion_images[ran_select]
                                 selected_arrays.append(lesion_images)
-                            if non_lesion_images.shape[0] > 0: 
                                 selected_arrays_non_lesion.append(non_lesion_images)
+                            # if non_lesion_images.shape[0] > 0: 
+                            #     selected_arrays_non_lesion.append(non_lesion_images)
         combined_array_lesion = np.concatenate(selected_arrays, axis=0)
         combined_array_non_lesion = np.concatenate(selected_arrays_non_lesion, axis=0)
         selected_indices = np.random.choice(combined_array_non_lesion.shape[0], size=combined_array_lesion.shape[0], replace=False)
@@ -498,7 +364,6 @@ def main():
 
         X_train, X_temp, y_train, y_temp = train_test_split(features, labels, test_size=0.4, random_state=42)
         x_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
-
         # Save the data
         np.save(f'X_train_{append_name}.npy', X_train)
         np.save(f'X_validation_{append_name}.npy', x_val)
@@ -506,143 +371,6 @@ def main():
         np.save(f'y_train_{append_name}.npy', y_train)
         np.save(f'y_validation_{append_name}.npy', y_val)
         np.save(f'y_test_{append_name}.npy', y_test)
-
-        #THIS PART IS DONE
-        # #create small medium and large patches
-        # create_small_medium_large(list_all_values)
-        # #plot hist
-        # histplot(list_all_values, bins='auto', kde=True, color='tab:blue')
-        # plt.xlabel('Euclidean Distance of the Bounding Box')
-        # plt.ylabel('Count')
-        # plt.savefig('hist_sizes_lesions.png',dpi=350)
-        # plt.close()
-    else:
-        X_train = np.load(f'X_train_{append_name}.npy')
-        X_test = np.load(f'X_test_{append_name}.npy')
-        y_train = np.load(f'y_train_{append_name}.npy')
-        y_test = np.load(f'y_test_{append_name}.npy')
-        x_val = np.load(f'X_validation_{append_name}.npy')
-        y_val = np.load(f'y_validation_{append_name}.npy')
-
-    print(f'x_train size {np.shape(X_train)}')
-    print(f'X_test size {np.shape(X_test)}')
-    print(f'y_train size {np.shape(y_train)}')
-    print(f'y_test size {np.shape(y_test)}')
-    print(f'x_val size {np.shape(x_val)}')
-    print(f'y_val size {np.shape(y_val)}')
-
-    plt.figure(figsize=(15, 8))
-    previous_val_acc = 0
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-
-    #Train custom
-    print('Train Custom Architecture')
-    if not exists(f'patch_custom_{append_name}_patch.h5'):
-        previous_val_acc = 0
-        patch_architecture = create_patch_custom_model()
-        for i in range(1):
-            history = patch_architecture.fit(X_train, y_train, epochs=100, batch_size=64,callbacks=[early_stopping],
-                                            validation_data=(x_val, y_val), verbose=1)
-
-            plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-            plt.plot(history.history['accuracy'], label=f'Training Accuracy ({i}th iteration)')
-            plt.plot(history.history['val_accuracy'], label=f'Validation Accuracy ({i}th iteration)')
-            #plt.plot(history.history['val_f1_score'], label=f'Validation F1 Score ({i}th iteration)')
-            plt.title(f'Custom Architecture Model Accuracy History {append_name} Patches')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.legend()
-
-            plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-            plt.plot(history.history['loss'], label=f'Training Loss ({i} iteration)')
-            plt.plot(history.history['val_loss'], label=f'Validation Loss ({i} iteration)')
-            plt.title(f'Custom Architecture Model Loss History {append_name} Patches')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.legend()
-            if history.history['val_accuracy'][-1] > previous_val_acc:
-                save_patch_model = patch_architecture
-                print(f'({i} iteration) best model: {history.history["val_accuracy"][-1]}')
-
-        plt.tight_layout()  # Adjust subplot spacing for better appearance
-        plt.savefig(f'training_accuracy_loss_custom_arch_{append_name}_patches.png', dpi=400)
-        test_results = patch_architecture.evaluate(X_test, y_test)
-        with open(f'test_results_custom_arch_{append_name}.txt', 'w') as file:
-            file.write(f'Test Accuracy: {test_results[1]}\n')
-            file.write(f'Test Loss: {test_results[0]}\n')
-        save_model(save_patch_model,f'patch_custom_{append_name}_patch.h5')
-
-    print('Train denseNet')
-    if not exists(f'patch_DenseNet121_{append_name}_patch.h5'):
-        patch_architecture_dense = create_patch_model_dense((int(WINDOW_SIZE/2),int(WINDOW_SIZE/2),3))
-        for i in range(1):
-            history = patch_architecture_dense.fit(X_train, y_train, epochs=100, batch_size=64,callbacks=[early_stopping],
-                                        validation_data=(x_val, y_val), verbose=1)
-
-            plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-            plt.plot(history.history['accuracy'], label=f'Training Accuracy ({i}th iteration)')
-            plt.plot(history.history['val_accuracy'], label=f'Validation Accuracy ({i}th iteration)')
-            #plt.plot(history.history['val_f1_score'], label=f'Validation F1 Score ({i}th iteration)')
-            plt.title(f'DenseNet Baseline Model Accuracy History {append_name} Patches ')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.legend()
-
-            plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-            plt.plot(history.history['loss'], label=f'Training Loss ({i} iteration)')
-            plt.plot(history.history['val_loss'], label=f'Validation Loss ({i} iteration)')
-            plt.title(f'Densenet Baseline Model Loss History {append_name} Patches')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.legend()
-            if history.history['val_accuracy'][-1] > previous_val_acc:
-                save_patch_model = patch_architecture_dense
-                print(f'({i} iteration) best model: {history.history["val_accuracy"][-1]}')
-        plt.tight_layout()  # Adjust subplot spacing for better appearance
-        plt.savefig(f'training_accuracy_loss_DenseNet_{append_name}_patch.png', dpi=400)
-        test_results = patch_architecture_dense.evaluate(X_test, y_test)
-        print(f'Test Accuracy: {test_results[1]}')
-        print(f'Test Loss: {test_results[0]}')
-        with open(f'test_results_dense_{append_name}.txt', 'w') as file:
-            file.write(f'Test Accuracy: {test_results[1]}\n')
-            file.write(f'Test Loss: {test_results[0]}\n')
-        save_model(save_patch_model,f'patch_DenseNet121_{append_name}_patch.h5')
-
-    if not exists(f'patch_ResNet152_{append_name}_patch.h5'):
-        print('Train ResNet')
-        previous_val_acc = 0
-        patch_architecture_res = create_patch_model_res((int(WINDOW_SIZE/2),int(WINDOW_SIZE/2),3))
-        for i in range(1):
-            history = patch_architecture_res.fit(X_train, y_train, epochs=100, batch_size=64,callbacks=[early_stopping],
-                                            validation_data=(x_val, y_val), verbose=1)
-
-            plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
-            plt.plot(history.history['accuracy'], label=f'Training Accuracy ({i}th iteration)')
-            plt.plot(history.history['val_accuracy'], label=f'Validation Accuracy ({i}th iteration)')
-            #plt.plot(history.history['val_f1_score'], label=f'Validation F1 Score ({i}th iteration)')
-            plt.title(f'ResNet152 Baseline Model Accuracy History {append_name} Patches')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.legend()
-
-            plt.subplot(1, 2, 2)  # 1 row, 2 columns, 2nd subplot
-            plt.plot(history.history['loss'], label=f'Training Loss ({i} iteration)')
-            plt.plot(history.history['val_loss'], label=f'Validation Loss ({i} iteration)')
-            plt.title(f'ResNet152 Baseline Model Loss History {append_name} Patches')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.legend()
-            if history.history['val_accuracy'][-1] > previous_val_acc:
-                save_patch_model = patch_architecture_res
-                print(f'({i} iteration) best model: {history.history["val_accuracy"][-1]}')
-
-        plt.tight_layout()  # Adjust subplot spacing for better appearance
-        plt.savefig(f'training_accuracy_loss_ResNet152_{append_name}_patches.png', dpi=400)
-        test_results = patch_architecture_res.evaluate(X_test, y_test)
-        with open(f'test_results_res_{append_name}.txt', 'w') as file:
-            file.write(f'Test Accuracy: {test_results[1]}\n')
-            file.write(f'Test Loss: {test_results[0]}\n')
-        save_model(save_patch_model,f'patch_ResNet152_{append_name}_patches.h5')
 
 if __name__ == "__main__":
     main()
